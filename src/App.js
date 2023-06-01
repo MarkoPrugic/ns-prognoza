3584a53eea5d2d70779de871a289efb668c0099e
Marko Prugic <markoprugic@hotmail.rs>
2023-06-01 18:23:29 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,69 +2,34 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
 import WeatherChart from './components/WeatherChart';
+import { weatherStatusMap } from './components/WeatherStatusMap';
 
 function App() {
-  const [temperature, setTemperature] = useState(0); // Trenutna temperatura
-  const [image, setImage] = useState(''); // Slika za prikaz vremenskih uslova
-  const [weatherStatus, setWeatherStatus] = useState(''); // Status vremena
-  const [forecast, setForecast] = useState([]); // Prognoza vremena
+  const [weatherData, setWeatherData] = useState({
+    temperature: 0,
+    image: '',
+    weatherStatus: '',
+    forecast: []
+  });
 
   useEffect(() => {
-    GetWeather();
+    getWeather();
   }, []);
 
-  const GetWeather = async () => {
+const getWeather = async () => {
     try {
-      const response = await fetch(process.env.REACT_APP_API_URL); // Uzimanje podataka o vremenu sa API-ja
-      const jsonData = await response.json(); // Parsiranje odgovora u JSON format
-      setTemperature(Math.round(jsonData.current_weather.temperature)); // Postavljanje trenutne temperature
-      
-      // Mapiranje vremenskih kodova na odgovarajući status i sliku
-      const weatherStatusMap = {
-        0:    { status: 'Čisto nebo',                                 image: '/res/sunny.svg' },
-        1:    { status: 'Uglavnom čisto',                             image: '/res/cloudy.svg' },
-        2:    { status: 'Delimično oblačno',                          image: '/res/cloudy.svg' },
-        3:    { status: 'Oblačno',                                    image: '/res/cloudy.svg' },
-        45:   { status: 'Maglovito',                                  image: '/res/fog.svg' },
-        48:   { status: 'Maglovito',                                  image: '/res/fog.svg' },
-        51:   { status: 'Sitna kisa',                                 image: '/res/drizzle.svg' },
-        53:   { status: 'Sitna kisa',                                 image: '/res/drizzle.svg' },
-        55:   { status: 'Sitna kisa',                                 image: '/res/drizzle.svg' },
-        56:   { status: 'Sitna kisa',                                 image: '/res/drizzle.svg' },
-        57:   { status: 'Sitna kisa',                                 image: '/res/drizzle.svg' },
-        61:   { status: 'Kisa - Blag intenzitet',                     image: '/res/rain.svg' },
-        63:   { status: 'Kisa - Umeren intenzitet',                   image: '/res/rain.svg' },
-        65:   { status: 'Kisa - Jak intenzitet',                      image: '/res/extreme-rain.svg' },
-        66:   { status: 'Ledena kisa - Lagan intenzitet',             image: '/res/sleet.svg' },
-        67:   { status: 'Ledena kisa - Jak intenzitet',               image: '/res/sleet.svg' },
-        71:   { status: 'Sneg',                                       image: '/res/snow.svg' },
-        73:   { status: 'Sneg',                                       image: '/res/snow.svg' },
-        75:   { status: 'Sneg',                                       image: '/res/snow.svg' },
-        77:   { status: 'Sneg',                                       image: '/res/snow.svg' },
-        80:   { status: 'Kišni pljuskovi - Blag intenzitet',          image: '/res/extreme-rain.svg' },
-        81:   { status: 'Kišni pljuskovi - Umeren intenzitet',        image: '/res/extreme-rain.svg' },
-        82:   { status: 'Kišni pljuskovi - Jak intenzitet',           image: '/res/extreme-rain.svg' },
-        85:   { status: 'Snežni pljuskovi - Blag intenzitet',         image: '/res/extreme-snow.svg' },
-        86:   { status: 'Snežni pljuskovi - Jak intenzitet',          image: '/res/extreme-snow.svg' },
-        95:   { status: 'Grmljavina',                                 image: '/res/thunderstorms.svg' },
-        96:   { status: 'Grmljavina',                                 image: '/res/thunderstorms.svg' },
-        99:   { status: 'Grmljavina',                                 image: '/res/thunderstorms.svg' },
-      };
+      const response = await fetch(process.env.REACT_APP_API_URL);
+      const jsonData = await response.json();
 
-      // Dobijanje statusa i slike na osnovu trenutnog vremenskog koda
-      const { status, image } = weatherStatusMap[jsonData.current_weather.weathercode];
-      setWeatherStatus(status);
-      setImage(image);
+      const { weathercode } = jsonData.current_weather;
+      const { status, image } = weatherStatusMap[weathercode];
 
-      // Izdvajanje podataka za prognozu iz dobijenih podataka
       const forecastTime = jsonData.hourly.time.slice(24, 72);
       const forecastTemperature = jsonData.hourly.temperature_2m.slice(24, 72);
       const forecastWeatherCode = jsonData.hourly.weathercode.slice(24, 72);
 
-      // Mapiranje vremenskih kodova prognoze na odgovarajući status
       const forecastWeather = forecastWeatherCode.map(code => weatherStatusMap[code].status);
 
-      // Kreiranje objekata sa podacima za prikaz na grafikonu
       const forecastData = forecastTime.map((time, index) => ({
         id: Math.random(),
         time,
@@ -72,12 +37,18 @@ function App() {
         weather: forecastWeather[index],
       }));
 
-      setForecast(forecastData); // Postavljanje podataka za prognozu
-    }
-    catch (error) {
-      console.error('greska: ', error); // Prikazivanje greške u konzoli
+      setWeatherData({
+        temperature: Math.round(jsonData.current_weather.temperature),
+        image,
+        weatherStatus: status,
+        forecast: forecastData
+      });
+    } catch (error) {
+      console.error('Error: ', error);
     }
   };
+  
+  const { temperature, image, weatherStatus, forecast } = weatherData;
 
   return (
     <div className="App">
--- src/components/WeatherChart.js
+++ src/components/WeatherChart.js
@@ -1,53 +1,51 @@
 import React from 'react';
 import moment from 'moment/moment';
 import 'moment/locale/sr';
-import {Box, Typography} from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { ResponsiveContainer, AreaChart, Area, XAxis, YAxis, Tooltip } from 'recharts';
 
-const WeatherChart = ({ forecast }) => {
-  // Komponenta za prilagođeni tooltip na grafikonu
-  const CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload && payload.length) {
-      const formattedLabel = moment(label).calendar();
-      const temperature = payload[0].value;
-      const weatherStatus = payload[1].value;
-      return (
-        <Box className="card-inner" sx={{
-          mx: 10,
-          mt: 16,
-          '& > :not(style)': { m: 4, width: '100%' },
-          background: 'rgba(43, 45, 66, 0.5)',
-          boxShadow: '0 0 15px rgba(43, 45, 66, 0.3)',
-        }}>
-          <Box sx={{}}>
-            <Typography>
-              {formattedLabel.charAt(0).toUpperCase() + formattedLabel.slice(1)}
-            </Typography>
-            <br />
-            <Typography variant='body2'>
-              {`${temperature} °C ${weatherStatus}`}
-            </Typography>
-          </Box>
+moment.locale('sr');
+
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    const [data] = payload;
+    const formattedLabel = moment(label).calendar();
+    const temperature = data.value;
+    const weatherStatus = data.payload.weather;
+    return (
+      <Box className="card-inner" sx={{
+        mx: 10,
+        mt: 16,
+        '& > :not(style)': { m: 4, width: '100%' },
+        background: 'rgba(43, 45, 66, 0.5)',
+        boxShadow: '0 0 15px rgba(43, 45, 66, 0.3)',
+      }}>
+        <Box sx={{}}>
+          <Typography>
+            {formattedLabel.charAt(0).toUpperCase() + formattedLabel.slice(1)}
+          </Typography>
+          <br />
+          <Typography variant='body2'>
+            {`${temperature} °C ${weatherStatus}`}
+          </Typography>
         </Box>
-      );
-    }
-    return null;
-  };
+      </Box>
+    );
+  }
+  return null;
+};
 
-  // Funkcija za formatiranje oznaka na X osi grafikona
-  const formatXAxis = (forecastData) => {
-    moment().locale("sr");
-    const formattedLabel = moment(forecastData).calendar();
-    return formattedLabel.charAt(0).toUpperCase() + formattedLabel.slice(1);
-  };
+const formatXAxis = (forecastData) => {
+  const formattedLabel = moment(forecastData).calendar();
+  return formattedLabel.charAt(0).toUpperCase() + formattedLabel.slice(1);
+};
 
-  // Funkcija za formatiranje oznaka na Y osi grafikona
-  const formatYAxis = (forecastData) => {
-    return forecastData + ' °C';
-  };
+const formatYAxis = (forecastData) => {
+  return forecastData + ' °C';
+};
 
-  return (
-    <ResponsiveContainer width="100%" height={400}>
+const WeatherChart = ({ forecast }) => (
+  <ResponsiveContainer width="100%" height={400}>
     <AreaChart data={forecast}>
       <defs>
         <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
@@ -77,6 +75,5 @@ const WeatherChart = ({ forecast }) => {
     </AreaChart>
   </ResponsiveContainer>
 );
-};
 
 export default WeatherChart;
